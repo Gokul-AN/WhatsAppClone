ca3861d3cf4b99cbce9ea511325b108e3e79a923
Gokul A N <angokul@CEIIND.COM>
2025-02-04 18:56:20 +0530

    initial commit

---
+++ Datas/index.js
@@ -0,0 +1,117 @@
+export const users = [
+    {
+        name:'Vijay',
+        messages:[
+            {
+                content:"Hi",
+                time: "02:01",
+                date:"02.02.2025",
+                sentBy: "me"
+            },
+            {
+                "content": "How are you?",
+                "time": "02:05",
+                "date": "02.02.2025",
+                "sentBy": "me"
+            },
+            {
+                "content": "I'm good, thanks!",
+                "time": "02:06",
+                "date": "02.02.2025",
+                "sentBy": "Vijay"
+            }
+        ]
+    },
+    {
+        "name": "Anita",
+        "messages": [
+            {
+                "content": "Hello",
+                "time": "10:15",
+                "date": "03.02.2025",
+                "sentBy": "me"
+            },
+            {
+                "content": "What's up?",
+                "time": "10:20",
+                "date": "03.02.2025",
+                "sentBy": "me"
+            },
+            {
+                "content": "Not much, just working.",
+                "time": "10:22",
+                "date": "03.02.2025",
+                "sentBy": "Anita"
+            }
+        ]
+    },
+    {
+        "name": "Rahul",
+        "messages": [
+            {
+                "content": "Hey",
+                "time": "15:30",
+                "date": "04.02.2025",
+                "sentBy": "me"
+            },
+            {
+                "content": "Are you coming to the party?",
+                "time": "15:35",
+                "date": "04.02.2025",
+                "sentBy": "me"
+            },
+            {
+                "content": "Yes, I'll be there!",
+                "time": "15:40",
+                "date": "04.02.2025",
+                "sentBy": "Rahul"
+            }
+        ]
+    },
+    {
+        "name": "Bumrah",
+        "messages": [
+            {
+                "content": "Good morning",
+                "time": "08:00",
+                "date": "05.02.2025",
+                "sentBy": "me"
+            },
+            {
+                "content": "Did you finish the report?",
+                "time": "08:10",
+                "date": "05.02.2025",
+                "sentBy": "me"
+            },
+            {
+                "content": "Yes, I sent it last night.",
+                "time": "08:15",
+                "date": "05.02.2025",
+                "sentBy": "Bumbrah"
+            }
+        ]
+    },
+    {
+        "name": "Suresh",
+        "messages": [
+            {
+                "content": "Hi there",
+                "time": "11:45",
+                "date": "06.02.2025",
+                "sentBy": "me"
+            },
+            {
+                "content": "Can we meet at 3 PM?",
+                "time": "11:50",
+                "date": "06.02.2025",
+                "sentBy": "me"
+            },
+            {
+                "content": "Sure, see you then!",
+                "time": "11:55",
+                "date": "06.02.2025",
+                "sentBy": "Suresh"
+            }
+        ]
+    }
+]
